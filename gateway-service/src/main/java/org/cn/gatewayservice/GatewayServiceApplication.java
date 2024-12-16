fb0ae5b78b67db910f2e650fdbdf1dc3c7608687
nouhaila <nohaila.chkhonty@gmail.com>
2024-12-16 22:17:55 +0100

    updating

---
+++ gateway-service/src/main/java/org/cn/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,26 @@
+package org.cn.gatewayservice;
+
+import org.cn.gatewayservice.configuration.RsaConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableConfigurationProperties(RsaConfig.class)
+public class GatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+
+    @Bean
+    DiscoveryClientRouteDefinitionLocator locator(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp){
+        return  new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
+    }
+
+}
---
+++ gateway-service/src/main/java/org/cn/gatewayservice/configuration/SecConfig.java
@@ -0,0 +1,43 @@
+package org.cn.gatewayservice.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+
+public class SecConfig {
+
+    private RsaConfig rsaConfig;
+
+    public SecConfig(RsaConfig rsaConfig) {
+        this.rsaConfig = rsaConfig;
+    }
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeExchange(auth -> auth
+                        .pathMatchers("/actuator/**", "/USER-SERVICE/actuator/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt());
+
+        return http.build();
+    }
+
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder() {
+        // Ensure that rsaConfig.publicKey() returns a valid PublicKey
+        return NimbusReactiveJwtDecoder.withPublicKey(rsaConfig.publicKey()).build();
+    }
+
+
+}
