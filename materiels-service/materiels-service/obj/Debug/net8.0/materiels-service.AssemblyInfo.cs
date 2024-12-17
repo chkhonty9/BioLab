95c412414d3f7a049aac59eb2ca931f6330bd224
nouhaila <nohaila.chkhonty@gmail.com>
2024-12-17 20:13:37 +0100

    adding config client and discovery client to project-service

--- materiels-service/materiels-service/obj/Debug/net8.0/materiels-service.AssemblyInfo.cs
+++ materiels-service/materiels-service/obj/Debug/net8.0/materiels-service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("materiels-service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+830c79a8115ba629acd6cf06901275122c9b6c67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+044defeba4d2358999cd4f0fb4ab4c20a2d2c379")]
 [assembly: System.Reflection.AssemblyProductAttribute("materiels-service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("materiels-service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
