4b5d667f4c4f62bf5c260e8c0838261e460f8f19
nouhaila <nohaila.chkhonty@gmail.com>
2024-12-21 23:16:51 +0100

    v1

--- gateway-service/src/main/java/org/cn/gatewayservice/configuration/RsaConfig.java
+++ gateway-service/src/main/java/org/cn/gatewayservice/configuration/RsaConfig.java
@@ -2,10 +2,10 @@ package org.cn.gatewayservice.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 
 
-@ConfigurationProperties(prefix = "rsa")
-
-public record RsaConfig(RSAPublicKey publicKey ) {
+@ConfigurationProperties("rsa")
+public record RsaConfig(RSAPublicKey publicKey, RSAPrivateKey privateKey){
 }
--- gateway-service/src/main/java/org/cn/gatewayservice/configuration/SecConfig.java
+++ gateway-service/src/main/java/org/cn/gatewayservice/configuration/SecConfig.java
@@ -2,15 +2,16 @@ package org.cn.gatewayservice.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
-
 public class SecConfig {
 
     private final RsaConfig rsaConfig;
@@ -21,6 +22,7 @@ public class SecConfig {
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeExchange(auth -> auth
@@ -31,21 +33,19 @@ public class SecConfig {
                                 "/MATERIELS-SERVICE/health",
                                 "/PROJECT-SERVICE/health",
                                 "/RESULTS-SERVICE/health").permitAll()
-                        .pathMatchers("/USER-SERVICE/**").hasAuthority("SCOPE_ADMIN")
-                        .pathMatchers("/MATERIELS-SERVICE/**").hasAnyAuthority("SCOPE_ADMIN", "SCOPE_BIOLOGIST")
-                        .pathMatchers("/PROJET-SERVICE/**").hasAnyAuthority("SCOPE_ADMIN", "SCOPE_BIOLOGIST")
+                        .pathMatchers("/USER-SERVICE/graphql").hasAuthority("SCOPE_ADMIN")
+                        .pathMatchers("/MATERIELS-SERVICE/graphql").hasAnyAuthority("SCOPE_ADMIN", "SCOPE_BIOLOGIST")
+                        .pathMatchers("/PROJET-SERVICE/graphql").hasAnyAuthority("SCOPE_ADMIN", "SCOPE_BIOLOGIST")
                         .pathMatchers("/RESULTS-SERVICE/**").hasAnyAuthority("SCOPE_ADMIN", "SCOPE_BIOLOGIST")
                         .anyExchange().authenticated()
                 )
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt());
-
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
         return http.build();
     }
 
 
     @Bean
     public ReactiveJwtDecoder jwtDecoder() {
-        // Ensure that rsaConfig.publicKey() returns a valid PublicKey
         return NimbusReactiveJwtDecoder.withPublicKey(rsaConfig.publicKey()).build();
     }
 
